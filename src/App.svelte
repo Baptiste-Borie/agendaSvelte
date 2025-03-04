edbdb2144870a4673aa221a83305866cda6b672a
Nassjet <nass.belkacem7@gmail.com>
2025-03-04 15:42:07 +0100

    add SideBarEvents, not perfect but works

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,7 @@
   import chevronNext from "./assets/next.png";
   import Login from "./lib/Login.svelte";
   import Week from "./lib/Week.svelte";
+  import SideBarEvents from "./lib/SideBarEvents.svelte";
   import {
     format,
     addMonths,
@@ -18,6 +19,7 @@
   let currentDate = new Date();
   let currentView = "month";
   let currentPage = "login";
+  let isOpen = false; //sidebar
 
   $: currentMonth = format(currentDate, "MMMM-yyyy", { locale: fr });
   $: currentMonthLabel = format(currentDate, "MMMM yyyy", { locale: fr });
@@ -66,7 +68,16 @@
       switchToMonthView();
     }
   }
+
+  function toggleSidebar() {
+        isOpen = !isOpen;
+  }
 </script>
+<!-- 
+<main>
+  <button on:click={toggleSidebar}>Open SideBar</button>
+  <SideBarEvents {isOpen}/>
+</main> -->
 
 <nav>
   {#if loggedInUser}
@@ -75,7 +86,12 @@
     <button onclick={() => navigate("login")}>Connexion</button>
   {/if}
   <button onclick={() => navigate("agenda")}>Agenda</button>
+  {#if loggedInUser}
+  <button onclick={toggleSidebar}>Créer un Evenement</button>
+  {/if}
 </nav>
+
+
 
 <main>
   {#if currentPage === "login"}
@@ -121,6 +137,7 @@
       {/key}
     {/if}
   {/if}
+  <SideBarEvents {isOpen}></SideBarEvents>
 </main>
 
 <style>
@@ -168,10 +185,5 @@
   img {
     width: 30px;
     height: 30px;
-  }
-
-  .button_event{
-    border-radius: 5px;
-    background-color: rgb(97, 96, 96);
   }
 </style>
