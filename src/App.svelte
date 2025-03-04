ff8abdad0ebfb9d56e103cb0aae85ed50fe607a9
Baptiste Borie <baptiste.borie@sports-decisions.com>
2025-03-04 12:42:46 +0100

    03_04 add views + various improves

--- src/App.svelte
+++ src/App.svelte
@@ -1,77 +1,124 @@
 <script>
-    import Month from "./lib/Month.svelte";
-    import chevronBack from "./assets/back.png";
-    import chevronNext from "./assets/next.png";
-    import {format, addMonths, subMonths} from "date-fns";
-    import {fr} from "date-fns/locale";
-    import Login from './lib/Login.svelte';
+  import Month from "./lib/Month.svelte";
+  import chevronBack from "./assets/back.png";
+  import chevronNext from "./assets/next.png";
+  import Login from "./lib/Login.svelte";
+  import Week from "./lib/Week.svelte";
+  import {
+    format,
+    addMonths,
+    subMonths,
+    startOfWeek,
+    endOfWeek,
+  } from "date-fns";
+  import { fr } from "date-fns/locale";
 
-
-    let currentPage = 'login';
-
-    let currentDate = new Date();
+  let currentDate = new Date();
+  let currentView = "month";
+  let currentPage = "login";
 
   $: currentMonth = format(currentDate, "MMMM-yyyy", { locale: fr });
   $: currentMonthLabel = format(currentDate, "MMMM yyyy", { locale: fr });
 
-    function previousMonth() {
-        currentDate = subMonths(currentDate, 1);
+  let loggedInUser = null;
+
+  function navigate(page) {
+    currentPage = page;
+  }
+
+  function handleLogin(user) {
+    loggedInUser = user;
+    navigate("agenda");
+  }
+
+  // Fonction pour gérer la déconnexion
+  function handleLogout() {
+    loggedInUser = null;
+    console.log("deco");
+    navigate("login");
+  }
+  function switchToMonthView() {
+    currentView = "month";
+  }
+
+  function switchToWeekView() {
+    currentView = "week";
+  }
+
+  function previousMonth() {
+    currentDate = subMonths(currentDate, 1);
+  }
+
+  function nextMonth() {
+    currentDate = addMonths(currentDate, 1);
+  }
+
+  $: startOfCurrentWeek = startOfWeek(currentDate, { weekStartsOn: 1 });
+  $: endOfCurrentWeek = endOfWeek(currentDate, { weekStartsOn: 1 });
+
+  function handleInputChange(event) {
+    const input = event.target.value.toLowerCase();
+    if (input.includes("semaine")) {
+      switchToWeekView();
+    } else if (input.includes("mois")) {
+      switchToMonthView();
     }
-
-    function nextMonth() {
-        currentDate = addMonths(currentDate, 1);
-    }
-
-    let loggedInUser = null;
-
-
-    function navigate(page) {
-        currentPage = page;
-    }
-
-    function handleLogin(user) {
-        loggedInUser = user;
-        navigate('agenda');
-    }
-
-    // Fonction pour gérer la déconnexion
-    function handleLogout() {
-        loggedInUser = null;
-        console.log("deco")
-        navigate('login');
-    }
+  }
 </script>
 
 <nav>
-    {#if loggedInUser}
-        <button onclick={handleLogout}>Déconnexion</button>
-    {:else}
-        <button onclick={() => navigate('login')}>Connexion</button>
-    {/if}
-    <button onclick={() => navigate('agenda')}>Agenda</button>
+  {#if loggedInUser}
+    <button onclick={handleLogout}>Déconnexion</button>
+  {:else}
+    <button onclick={() => navigate("login")}>Connexion</button>
+  {/if}
+  <button onclick={() => navigate("agenda")}>Agenda</button>
 </nav>
 
 <main>
-    {#if currentPage === 'login'}
-        <Login onLogin={handleLogin} onLogout={handleLogout} />
-    {:else if currentPage === 'agenda'}
-        <header>
-            <div>
-                <button onclick={previousMonth}>
-                    <img src={chevronBack} alt="Back" />
-                </button>
-                <button onclick={nextMonth}>
-                    <img src={chevronNext} alt="Next" />
-                </button>
-            </div>
-            <h1>
-                {currentMonthLabel}
-            </h1>
-        </header>
-        {#key currentMonth}
-            <Month {currentMonth} />
-        {/key}
+  {#if currentPage === "login"}
+    <Login onLogin={handleLogin} onLogout={handleLogout} />
+  {:else if currentPage === "agenda"}
+    <header class="header">
+      <div class="monthSection">
+        <div class="buttons">
+          <button
+            class="todayButton"
+            onclick={() => (currentDate = new Date())}
+          >
+            Aujourd'hui
+          </button>
+          <button onclick={previousMonth}>
+            <img src={chevronBack} alt="Back" />
+          </button>
+          <button onclick={nextMonth}>
+            <img src={chevronNext} alt="Next" />
+          </button>
+        </div>
+        <h1>{currentMonthLabel}</h1>
+      </div>
+      <div>
+        <select
+          name="viewSelector"
+          id="viewSelector"
+          oninput={handleInputChange}
+        >
+          <option value="mois">Mois</option>
+          <option value="semaine">Semaine</option>
+        </select>
+      </div>
+    </header>
+
+    {#if currentView === "month"}
+      {#key currentMonth}
+        <Month {currentMonth} />
+      {/key}
+    {:else if currentView === "week"}
+      {#key startOfCurrentWeek}
+        <Week {currentMonth} />
+      {/key}
     {/if}
+  {/if}
 </main>
 
 <style>
@@ -81,11 +128,34 @@
     justify-content: center;
     align-items: center;
   }
-  header {
+  .header {
     display: flex;
     justify-content: space-between;
     align-items: center;
     width: 100%;
+  }
+  h1 {
+    font-size: 2rem;
+  }
+  .monthSection {
+    display: flex;
+    align-items: center;
+  }
+  .todayButton {
+    border: 1px solid rgb(69, 69, 69);
+    color: rgb(69, 69, 69);
+    padding: 10px;
+    text-align: center;
+    cursor: pointer;
+  }
+  .todayButton:hover {
+    background-color: rgb(69, 69, 69);
+    transition: 0.3s;
+    color: white;
+  }
+  .buttons {
+    display: flex;
+    align-items: center;
   }
   button {
     background: none;
--- src/lib/Cellule.svelte
+++ src/lib/Cellule.svelte
@@ -1,10 +1,20 @@
 <script>
   export let date;
+  export let today;
+  export let view;
+
+  const isToday = (date) => {
+    const todayDate = today.setHours(0, 0, 0, 0);
+    const targetDate = date.setHours(0, 0, 0, 0);
+    return todayDate === targetDate;
+  };
+
+  const isWeekView = view === "Week";
 </script>
 
-<div class="cellule">
+<div class={"cellule " + (isWeekView ? " week" : "month")}>
   {#if date}
-    <span>{date.getDate()}</span>
+    <span class={isToday(date) ? "today" : ""}>{date.getDate()}</span>
   {/if}
 </div>
 
@@ -12,14 +22,29 @@
   .cellule {
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: start;
     border: 1px solid #ddd;
-    min-height: 100px;
-    min-width: 100px;
     cursor: pointer;
   }
 
+  .week {
+    min-height: 612px;
+    min-width: 125px;
+  }
+
+  .month {
+    min-height: 100px;
+    min-width: 125px;
+  }
   .cellule:hover {
     background-color: #f0f0f0;
+  }
+
+  .today {
+    background-color: #0b57d0;
+    color: white;
+    border-radius: 99%;
+    width: 25px;
+    padding: 1px;
   }
 </style>
